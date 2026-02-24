41122cbe4ab431673cad46611e7fb3e7f5618e78
Hari <vharikrishnan532@gmail.com.com>
2026-02-24 08:47:02 +0530

    media and app settings changed

--- StackShop_project/settings.py
+++ StackShop_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'seller',
     'customer',
 ]
+AUTH_USER_MODEL = 'core.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -120,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[BASE_DIR/'static']
+MEDIA_URL = '/media/'
+MEDIA_ROOT=BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
