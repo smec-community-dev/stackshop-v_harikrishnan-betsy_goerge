5c770c79c6b2ca34a90d4447f88165bfadc86ff7
Betsy <betsygeorge1124@gmail.com.com>
2026-02-25 13:51:23 +0530

    seller profile added

--- seller/urls.py
+++ seller/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('seller_bridge/', views.seller_bridge, name='seller-bridge'),
+    path('sellerprofile',views.seller_profile_view,name='seller-profile')
+
 ]
--- seller/views.py
+++ seller/views.py
@@ -5,12 +5,26 @@ from .models import SellerProfile
 
 @login_required
 def seller_profile_view(request):
-    profile = SellerProfile.objects.filter(user=request.user).first()
+    profile, created = SellerProfile.objects.get_or_create(user=request.user)
 
-    if not profile:
-        return redirect("login")
+    if request.method == "POST":
 
-    return render(request, "seller/sellerprofilepage.html", {"profile": profile})
+        profile.store_name = request.POST.get("store_name")
+        profile.store_slug = request.POST.get("store_slug")
+        profile.gst_number = request.POST.get("gst_number")
+        profile.pan_number = request.POST.get("pan_number")
+        profile.bank_account_number = request.POST.get("bank_account_number")
+        profile.ifsc_code = request.POST.get("ifsc_code")
+        profile.business_address = request.POST.get("business_address")
+
+        
+        if request.FILES.get("store_image"):
+            profile.store_image = request.FILES.get("store_image")
+
+        profile.save()
+        return redirect('seller-profile')
+
+    return render(request, "seller_templates/sellerprofilepage.html", {"profile": profile})
 
 def seller_bridge(request):
     user=request.user
